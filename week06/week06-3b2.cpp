1b08735f91a09cf3186cd186f2a179caac9fb937
Jeng-Sheng Yeh 葉正聖 <jsyeh.org@gmail.com>
2025-10-15 11:44:56 +0800

    week06

---
+++ week06/week06-3b2.cpp
@@ -0,0 +1,19 @@
+// week06-3b.cpp
+// SOIT106_ADVANCE_009
+#include <iostream>
+using namespace std;
+
+void f(int n)
+{
+	if(n==0) return;
+	cout << "now n:" << n
+	     << " skin: "<< n%10 <<"\n";
+	f(n/10);
+}
+int main()
+{
+	int n;
+	cin >> n;
+	f(n);
+	cout << "\n";
+}
